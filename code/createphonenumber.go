4212045209ecea0b7128b0c49faa5e36b8896229
keyadaniel56 <keyadaniel56@gmail.com>
2026-04-28 00:44:09 +0100

    feat : createphonnumber

---
+++ code/createphonenumber.go
@@ -0,0 +1,23 @@
+// package code
+
+// import (
+// 	"fmt"
+// )
+
+// func CreatePhoneNumber(numbers [10]uint) string {
+// 	return fmt.Sprintf("(%d%d%d) %d%d%d-%d%d%d%d",
+// 		numbers[0], numbers[1], numbers[2],
+// 		numbers[3], numbers[4], numbers[5],
+// 		numbers[6], numbers[7], numbers[8], numbers[9])
+// }
+
+package code
+
+import "fmt"
+
+func CreatePhoneNumber(numbers [10]uint) string {
+	return fmt.Sprintf("(%d%d%d) %d%d%d-%d%d%d%d",
+		numbers[0], numbers[1], numbers[2],
+		numbers[3], numbers[4], numbers[5],
+		numbers[6], numbers[7], numbers[8], numbers[9])
+}
